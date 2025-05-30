3b16090a3b46041e4e8bde3cc22d041d30aacb59
ARYAN-095 <aryanshukla095@gmail.com>
2025-05-30 19:34:48 +0000

    setup clerk

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import {useUser } from '@clerk/nextjs'
+import React from 'react' 
+
+const DashboardPage=()=>{
+    const {user}= useUser()
+    return (
+        <div> {user?.firstName}</div>
+    )
+}
